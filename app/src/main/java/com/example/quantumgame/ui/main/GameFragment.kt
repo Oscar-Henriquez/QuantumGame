dc86022cddaeeb8fcc0847868893c7d520ba271a
Oscar-Henriquez <52573244+Oscar-Henriquez@users.noreply.github.com>
2019-10-19 12:22:35 -0400

    Initial commit

---
+++ app/src/main/java/com/example/quantumgame/ui/main/GameFragment.kt
@@ -0,0 +1,26 @@
+package com.example.quantumgame.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.quantumgame.R
+import kotlinx.android.synthetic.main.main_fragment.*
+
+class GameFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = GameFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.game_fragment, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/quantumgame/ui/main/MainFragment.kt
@@ -0,0 +1,62 @@
+package com.example.quantumgame.ui.main
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.quantumgame.R
+import android.widget.Toast
+import kotlinx.android.synthetic.main.main_fragment.*
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // set on-click listener
+        startButton.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            startGame()
+            Toast.makeText(this.context, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+    }
+
+    private fun startGame() {
+        // Get the text fragment instance
+        val gameFragment = GameFragment()
+
+        // Get the support fragment manager instance
+        val manager = activity?.supportFragmentManager
+
+        // Begin the fragment transition using support fragment manager
+        val transaction = manager?.beginTransaction()
+
+        // Replace the fragment on container
+        transaction?.replace(R.id.main,gameFragment)
+        transaction?.addToBackStack(null)
+
+        // Finishing the transition
+        transaction?.commit()
+    }
+
+}
